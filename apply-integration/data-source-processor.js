44122c3552256740a3952672b49b9b201da44e85
Daniels Jirgensons <djirgensons@evolution.com>
2024-01-30 14:32:12 +0200

    source filtering by categories

--- apply-integration/data-source-processor.js
+++ apply-integration/data-source-processor.js
@@ -1,20 +1,8 @@
 class Source {
     constructor() {
-        this.sourceTypeId = 'ORGANIC';
-        this.sourceSubTypeId = 'CAREER_SITE';
-        this.sourceId = 'default_source_id';
-    }
-
-    getReferral(data) {
-        let referral = '';
-
-        try {
-            referral = JSON.parse(data);
-        } catch (e) {
-            referral = null;
-        }
-
-        return referral;
+        this.sourceTypeId = 'PAID';
+        this.sourceSubTypeId = 'BOARD';
+        this.sourceId = '11bb413e-6c88-459f-84ca-3049cefb1450';
     }
 
     getSource(data) {
@@ -35,11 +23,65 @@ class Source {
             return this.filterSource(source);
         }
 
+        console.log(data);
         return this;
     }
 
+    getReferral(data) {
+        let referral = '';
+
+        try {
+            referral = JSON.parse(data);
+        } catch (e) {
+            referral = null;
+        }
+
+        return referral;
+    }
+
     filterSource(source) {
-        console.log('filterSource');
-        console.log(source);
+        // console.log('filterSource');
+        // console.log(source);
+
+        switch (true) {
+            case /\b(google|gclid)\b/i.test(source): // Google
+                this.sourceId = 'google_source_id';
+                break;
+            case /\b(facebook|fbclid)\b/i.test(source): // Facebook
+                this.sourceId = 'facebook_source_id';
+                break;
+            // case /\binstagram\b/i.test(source):
+            case /instagram/i.test(source) && !/fbclid/i.test(source): // Instagram
+                this.sourceId = 'instagram_source_id';
+                break;
+            case /\b(linkedin|lnkd)\b/i.test(source): // Linkedin
+                this.sourceId = 'linkedin_source_id';
+                break;
+            case /\bsnapchat\b/i.test(source): // Snapchat
+                this.sourceId = 'snapchat_source_id';
+                break;
+            case /\btiktok\b/i.test(source): // TikTok
+                this.sourceId = 'tiktok_source_id';
+                break;
+            case /cv\.lv/i.test(source): // cv.lv
+                this.sourceId = 'cv_lv_source_id';
+                break;
+            case /jobs\.bg/i.test(source): // jobs.bg
+                this.sourceId = 'jobs_bg_source_id';
+                break;
+            case /danskespil\.dk/i.test(source): // danskespil.dk
+                this.sourceId = 'danskespil_dk_source_id';
+                break;
+            case /\btvnet\b/i.test(source): // tvnet
+                this.sourceId = 'tvnet_source_id';
+                break;
+            case /\btinder\b/i.test(source): // Tinder
+                this.sourceId = 'tinder_source_id';
+                break;
+            default:
+                console.log(source);
+        }
+
+        return this;
     }
 }
