07d8faf311c5d1876b86e467b04eb2438fdd0dd1
Daniels Jirgensons <daniels.jirgensons@prof-designs.lv>
2024-03-26 15:49:27 +0200

    add new source for GE, LV, other

--- apply-integration/data-source-processor.js
+++ apply-integration/data-source-processor.js
@@ -67,12 +67,16 @@ class Source {
                 this.sourceLabel = 'google';
                 this.sourceId = 'd74ccbf5-53b2-4024-b5f1-475643dd2e14';
                 break;
+            case /\bbing\b/i.test(source):  // Bing
+                this.sourceLabel = 'bing';
+                this.sourceId = '28934fe0-0db1-4637-b28c-5fd4352ef40e';
+                break;
             case /\b(facebook|fbclid)\b/i.test(source) && !/instagram/i.test(source): // Facebook
                 this.sourceLabel = 'facebook';
                 this.sourceId = '2e635be6-2d38-414f-90d5-9bb99f86c908';
                 break;
-            case /\binstagram\b/i.test(source):
-                // case /instagram/i.test(source) && !/fbclid/i.test(source): // Instagram
+            case /\binstagram\b/i.test(source): // Instagram
+                // case /instagram/i.test(source) && !/fbclid/i.test(source):
                 this.sourceLabel = 'instagram';
                 this.sourceId = '115b0cb6-efac-4c5e-b039-6bcdf8bc1fd3';
                 break;
@@ -87,10 +91,6 @@ class Source {
             case /\btiktok\b/i.test(source): // TikTok
                 this.sourceLabel = 'tiktok';
                 this.sourceId = '3af3457e-f119-4f1d-927b-c6935e52f03d';
-                break;
-            case /cv\.lv/i.test(source): // cv.lv
-                this.sourceLabel = 'cv.lv';
-                this.sourceId = '91fa9219-4fd1-47af-aa8b-dc985fd2c5fd';
                 break;
             case /jobs\.bg/i.test(source): // jobs.bg
                 this.sourceLabel = 'jobs.bg';
@@ -113,17 +113,53 @@ class Source {
                 this.sourceId = '45c74c79-9e64-44e7-a7d2-e2ecdfaa6d25';
                 break;
             // TEST: could be ?utm_source=studentjob
-            case /\bstudentjob.se\b/i.test(source): // studentjob.se
+            case /\bstudentjob\.se\b/i.test(source): // studentjob.se
                 this.sourceLabel = 'studentjob.se';
                 this.sourceId = 'c71c4475-cd3f-406b-8120-a11da9ccbd94';
+                break;
+            case /ejobs\.ro/i.test(source): // ejobs.ro
+                this.sourceLabel = 'ejobs.ro';
+                this.sourceId = '872ca014-f86f-4790-8999-4a1cfb4bf686';
+                break;
+            
+            /**
+             * Latvia specific sources
+             */
+            case /cv\.lv/i.test(source): // cv.lv
+                this.sourceLabel = 'cv.lv';
+                this.sourceId = '91fa9219-4fd1-47af-aa8b-dc985fd2c5fd';
                 break;
             case /\bvisidarbi\b/i.test(source): // visidarbi.lv
                 this.sourceLabel = 'visidarbi.lv';
                 this.sourceId = 'e9f93478-6492-4267-bb2f-78149705b880';
                 break;
-            case /ejobs\.ro/i.test(source): // ejobs.ro
-                this.sourceLabel = 'ejobs.ro';
-                this.sourceId = '872ca014-f86f-4790-8999-4a1cfb4bf686';
+            case /\bprakse\.lv\b/i.test(source): // prakse.lv
+                this.sourceLabel = 'prakse.lv';
+                this.sourceId = '6b33ee91-ee14-4fdd-b243-881d3e38876c';
+                break;
+            case /\b(ss\.lv|m\.ss\.com|ss\.com)\b/i.test(source): // ss.lv
+                this.sourceLabel = 'ss.lv';
+                this.sourceId = 'a53a5166-825a-409c-b192-a385dbea630f';
+                break;
+            
+            /**
+             * Georgia specific sources
+             */
+            case /hr\.ge/i.test(source): // hr.ge
+                this.sourceLabel = 'hr.ge';
+                this.sourceId = 'c2a4e782-f68f-4365-99f3-2ad0b1392809';
+                break;
+            case /awork\.ge/i.test(source): // awork.ge
+                this.sourceLabel = 'awork.ge';
+                this.sourceId = '0a6a6164-66c0-439f-9ae4-d47dc1ed9847';
+                break;
+            case /unijobs\.ge/i.test(source): // unijobs.ge
+                this.sourceLabel = 'unijobs.ge';
+                this.sourceId = 'fde45033-2279-43b2-94c7-8f8f4be535a2';
+                break;
+            case /jobs\.ge/i.test(source): // jobs.ge
+                this.sourceLabel = 'jobs.ge';
+                this.sourceId = 'a39c9cfa-d0ab-427f-84ae-5025de9ed6ff';
                 break;
         }
 
